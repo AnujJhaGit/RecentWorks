f3be827a9cf3a55d7e5c693db17e7a4e88917706
Anuj jha <jhaanuj2003@gmail.com>
2023-01-02 16:37:50 +0530

    changes

--- CodeBeautify/image flipper/app.js
+++ CodeBeautify/image flipper/app.js
@@ -1,68 +1,67 @@
-var loadfile = function (event) {
-    var img = document.getElementById('img');
-    img.src = URL.createObjectURL(event.target.files[0]);
-    img.onload = function(){
-        URL.revokeObjectURL(img.src)
-        img.style.transform = null
-    }   
- }
+var canvas = document.getElementById('c');
+var ctx = canvas.getContext('2d');
+var img = new Image();
 
+
+img.addEventListener('load', function() {
+    ctx.filter = 'none';
+    canvas.width = this.naturalWidth;
+    canvas.height = this.naturalHeight;
+    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img, 0, 0, this.width,this.height);
+    window.URL.revokeObjectURL(this.src);
+  }, false);
+
+
+// var loadfile = function (event) {
+//     img.src = URL.createObjectURL(event.target.files[0]);
+//     img.onload = function(){
+//         img.style.transform = null
+//     }   
+//  }
+ 
  const flipH = document.getElementById('flipH')
  const flipV = document.getElementById('flipV')
  
  flipH.addEventListener('click', flipHH)
  flipV.addEventListener('click', flipVV)
 function flipHH(e){
-    if(img.style.webkitTransform === ''){
-        img.style.webkitTransform = 'scaleY(-1)'
-    }else if(img.style.webkitTransform === 'scaleY(-1)'){
-        img.style.webkitTransform = ''
-    }else if(img.style.webkitTransform === 'scaleX(-1)') {
-        img.style.webkitTransform = 'scale(-1, -1)'
-    }else if(img.style.webkitTransform === 'scale(-1, -1)'){
-        img.style.webkitTransform = 'scaleX(-1)'
+console.log(canvas.style)
+
+    if(canvas.style.transform === ''){
+        canvas.style.transform = 'scaleY(-1)'
+    }else if(canvas.style.transform === 'scaleY(-1)'){
+        canvas.style.transform = ''
+    }else if(canvas.style.transform === 'scaleX(-1)') {
+        canvas.style.transform = 'scale(-1, -1)'
+    }else if(canvas.style.transform === 'scale(-1, -1)'){
+        canvas.style.transform = 'scaleX(-1)'
     }
+
 }
 
 function flipVV(e){
-    if(img.style.webkitTransform === ''){
-        img.style.webkitTransform = 'scaleX(-1)'
-    }else if(img.style.webkitTransform === 'scaleX(-1)'){
-        img.style.webkitTransform = ''
-    }else if(img.style.webkitTransform === 'scaleY(-1)') {
-        img.style.webkitTransform = 'scale(-1,-1)'
-    }else if(img.style.webkitTransform === 'scale(-1, -1)'){
-        img.style.webkitTransform = 'scaleY(-1)'
+    if(canvas.style.transform === ''){
+        canvas.style.transform = 'scaleX(-1)'
+    }else if(canvas.style.transform === 'scaleX(-1)'){
+        canvas.style.transform = ''
+    }else if(canvas.style.transform === 'scaleY(-1)') {
+        canvas.style.transform = 'scale(-1,-1)'
+    }else if(canvas.style.transform === 'scale(-1, -1)'){
+        canvas.style.transform = 'scaleY(-1)'
     }
 }
-
-
-// function downloadimg(){
-// //  document.getElementById('button').href = "img."
-// console.log(img.src)
-
-// axios({
-//     url: img.src,
-//     method:'GET',
-//     responseType: 'blob'
-// })
-// .then((response) => {
-//    const url = window.URL
-//    .createObjectURL(new Blob([response.data]));
-//           const link = document.getElementById('button');
-//           link.href = url;
-//           link.setAttribute('download', 'image.jpg');
-//           link.click();
-// })
-// }
-
+function handleFiles(files) {
+  if (files.length) {
+    img.src = window.URL.createObjectURL(files[0]);
+  }
+}
 var download = document.createElement('a');
-download.innerHTML = 'download';
-download.addEventListener('click', function(e){
-    download.href = img.src;
-    download.download = 'img.png';
-}, false)
-
+download.innerHTML = 'Download the image';
+download.addEventListener('click', function(ev) {
+  download.href = canvas.toDataURL();
+  download.download = 'img.png';
+    console.log(canvas.toDataURL())
+}, false);
 const btn = document.getElementById('button');
-console.log(btn)
 btn.appendChild(download)
