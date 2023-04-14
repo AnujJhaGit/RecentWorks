112dd89b25f0c5c0d475187926ddc5374ee4c064
Anuj jha <jhaanuj2003@gmail.com>
2023-04-14 20:01:17 +0530

    changes

--- CodeBeautify/Json to proto convertor/json-proto/src/highlight.ts
+++
@@ -1,8 +0,0 @@
-import hljs from "highlight.js/lib/core";
-import protobuf from "highlight.js/lib/languages/protobuf";
-
-hljs.registerLanguage("protobuf", protobuf);
-
-export default function highlight(source: string): string {
-    return hljs.highlight(source, {language: "protobuf"}).value;
-}
