ee01a94b489a0eea2a56f8b6d5e123d9137c17fd
Anuj jha <jhaanuj2003@gmail.com>
2023-04-23 22:37:39 +0530

    changes

--- CodeBeautify/PunnyCode Convertor/app.js
+++ CodeBeautify/PunnyCode Convertor/app.js
@@ -1,9 +1,17 @@
-const inputArea = document.getElementById('inputarea');
-inputArea.selectionStart = 0;
-inputArea.selectionEnd = 0;
-const outputArea = document.getElementById('outputarea');
-outputArea.selectionStart = 0;
-outputArea.selectionEnd = 0;
+const inputArea = document.getElementById("inputarea");
+const outputArea = document.getElementById("outputarea");
+const autoPreviewCheckbox = document.getElementById("autoPreview");
+
+inputArea.addEventListener('input',function(){
+  if(autoPreviewCheckbox.checked){
+    TextToPunny();
+  }
+})
+outputArea.addEventListener('input',function(){
+  if(autoPreviewCheckbox.checked){
+    PunnyToText();
+  }
+})
 function TextToPunny() {
   const inputText = inputArea.value;
   if (/[\u0080-\uffff]/.test(inputText)) {
@@ -14,11 +22,16 @@ function TextToPunny() {
   }
 }
 function PunnyToText() {
-  const punycodeText = outputArea.value; 
+  const punycodeText = outputArea.value;
   try {
     const inputText = punycode.toUnicode(punycodeText);
     inputArea.value = inputText;
   } catch (err) {
     inputArea.value = punycodeText;
   }
+}
+function copyText(id) {
+  var textarea = document.getElementById(id);
+  textarea.select();
+  document.execCommand("copy");
 }
