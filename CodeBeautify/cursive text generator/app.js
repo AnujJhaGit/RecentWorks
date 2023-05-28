e1303a777089431179fedb19f582ffbc19eb8aba
Anuj jha <jhaanuj2003@gmail.com>
2023-05-28 11:51:12 +0530

    changes

---
+++ CodeBeautify/cursive text generator/app.js
@@ -0,0 +1,89 @@
+const buttons = document.querySelectorAll('.button');
+const inputText = document.getElementById("input-text");
+const outputText = document.getElementById('output-text');
+let activeButton = null;
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (activeButton) {
+      activeButton.classList.remove('is-primary');
+    }
+    button.classList.add('is-primary');
+    activeButton = button;
+    updateOutput();
+    applyFormat(button.id);
+    console.log('Button clicked:', button.id);
+  });
+});
+
+const formatStyles = {
+  'cursive-button': {
+    fontFamily: 'cursive',
+    fontWeight: '',
+    textDecoration: '',
+    fontStyle: ''
+  },
+  'bold-button': {
+    fontFamily: '',
+    fontWeight: 'bold',
+    textDecoration: '',
+    fontStyle: ''
+  },
+  'strike-through-button': {
+    fontFamily: '',
+    fontWeight: '',
+    textDecoration: 'line-through',
+    fontStyle: ''
+  },
+  'underline-button': {
+    fontFamily: '',
+    fontWeight: '',
+    textDecoration: 'underline',
+    fontStyle: ''
+  },
+  'italics-button': {
+    fontFamily: '',
+    fontWeight: '',
+    textDecoration: '',
+    fontStyle: 'italic'
+  },
+  'bold-italic-button': {
+    fontFamily: '',
+    fontWeight: 'bold',
+    textDecoration: '',
+    fontStyle: 'italic'
+  },
+  'bold-cursive-button': {
+    fontFamily: 'cursive',
+    fontWeight: 'bold',
+    textDecoration: '',
+    fontStyle: ''
+  },
+  'sans-bold-italic-button': {
+    fontFamily: 'sans-serif',
+    fontWeight: 'bold',
+    textDecoration: '',
+    fontStyle: 'italic'
+  },
+  'serif-bold-italic-button': {
+    fontFamily: 'serif',
+    fontWeight: 'bold',
+    textDecoration: '',
+    fontStyle: 'italic'
+  }
+};
+
+function applyFormat(buttonId) {
+  const format = formatStyles[buttonId] || {};
+  outputText.style.fontFamily = format.fontFamily || '';
+  outputText.style.fontWeight = format.fontWeight || '';
+  outputText.style.textDecoration = format.textDecoration || '';
+  outputText.style.fontStyle = format.fontStyle || '';
+}
+
+inputText.addEventListener('input', updateOutput);
+
+function updateOutput() {
+  outputText.textContent = inputText.value;
+  applyFormat(activeButton ? activeButton.id : null);
+}
